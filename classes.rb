140b068d384a036afa46a4c40ef9a4bffa9bd800
Lauren Harradine <lhharradine@gmail.com>
2022-04-06 20:40:09 +1000

    Added another class

--- classes.rb
+++ classes.rb
@@ -25,3 +25,45 @@ class Computer
   other_comp.create("nigelfile.xls")
   
   puts Computer.get_users
+
+  class Account
+    attr_reader :name
+    attr_reader :balance
+    
+    def initialize(name, balance=100)
+      @name = name
+      @balance = balance
+    end
+  
+    def display_balance(pin_number)
+      if pin_number == pin
+        puts "Balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+  
+    def withdraw(pin_number, amount)
+      if pin_number == pin
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+  
+    private
+    def pin
+      @pin = 1234
+    end
+  
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+  end
+  
+  checking_account = Account.new("LozMoney", 100000)
+  checking_account.display_balance(1234)
+  checking_account.display_balance(1253)
+  checking_account.withdraw(14213, 500)
+  checking_account.withdraw(1234, 500)
